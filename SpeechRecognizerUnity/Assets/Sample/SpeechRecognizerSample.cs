7a8d7b891db4f17bfbabb6c8c202db473d89b21c
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-02-12 15:16:54 +0100

    Initial Unity Project

---
+++ SpeechRecognizerUnity/Assets/Sample/SpeechRecognizerSample.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpeechRecognizerPlugin
+{
+    public class SpeechRecognizerSample : MonoBehaviour
+    {
+        [SerializeField] Text resultText;
+        [SerializeField] Button startButton;
+
+        void Start()
+        {
+
+        }
+
+        
+
+    }
+}
+
