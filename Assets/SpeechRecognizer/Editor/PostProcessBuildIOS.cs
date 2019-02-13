023211237ca70c16d5f2088ee077bac0129c79a9
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-02-13 21:18:05 +0100

    Add namespace

--- Assets/SpeechRecognizer/Editor/PostProcessBuildIOS.cs
+++ Assets/SpeechRecognizer/Editor/PostProcessBuildIOS.cs
@@ -4,43 +4,48 @@ using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
 
-public static class PostProcessBuildIOS
+namespace SpeechRecognizerPlugin.Editor
 {
 
-    [PostProcessBuild]
-    public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
+
+    public static class PostProcessBuildIOS
     {
-        if (buildTarget != BuildTarget.iOS)
+
+        [PostProcessBuild]
+        public static void ChangeXcodePlist(BuildTarget buildTarget, string pathToBuiltProject)
         {
-            return;
+            if (buildTarget != BuildTarget.iOS)
+            {
+                return;
+            }
+
+            EditInfoPlist(Path.Combine(pathToBuiltProject, "Info.plist"));
+            AddFrameworks(PBXProject.GetPBXProjectPath(pathToBuiltProject));
         }
 
-        EditInfoPlist(Path.Combine(pathToBuiltProject, "Info.plist"));
-        AddFrameworks(PBXProject.GetPBXProjectPath(pathToBuiltProject));
-    }
+        static void EditInfoPlist(string plistPath)
+        {
+            PlistDocument plist = new PlistDocument();
+            plist.ReadFromString(File.ReadAllText(plistPath));
 
-    static void EditInfoPlist(string plistPath)
-    {
-        PlistDocument plist = new PlistDocument();
-        plist.ReadFromString(File.ReadAllText(plistPath));
+            // Get root
+            PlistElementDict rootDict = plist.root;
+            rootDict.SetString("NSMicrophoneUsageDescription", "To use speech recognization.");
+            rootDict.SetString("NSSpeechRecognitionUsageDescription", "To use speech recognization.");
 
-        // Get root
-        PlistElementDict rootDict = plist.root;
-        rootDict.SetString("NSMicrophoneUsageDescription", "To use speech recognization.");
-        rootDict.SetString("NSSpeechRecognitionUsageDescription", "To use speech recognization.");
+            // Save updated
+            File.WriteAllText(plistPath, plist.WriteToString());
+        }
 
-        // Save updated
-        File.WriteAllText(plistPath, plist.WriteToString());
-    }
+        static void AddFrameworks(string projectPath)
+        {
+            PBXProject project = new PBXProject();
+            project.ReadFromString(File.ReadAllText(projectPath));
+            string targetGUID = project.TargetGuidByName(PBXProject.GetUnityTargetName());
 
-    static void AddFrameworks(string projectPath)
-    {
-        PBXProject project = new PBXProject();
-        project.ReadFromString(File.ReadAllText(projectPath));
-        string targetGUID = project.TargetGuidByName(PBXProject.GetUnityTargetName());
+            project.AddFrameworkToProject(targetGUID, "Speech.framework", false);
 
-        project.AddFrameworkToProject(targetGUID, "Speech.framework", false);
-
-        File.WriteAllText(projectPath, project.WriteToString());
+            File.WriteAllText(projectPath, project.WriteToString());
+        }
     }
 }
